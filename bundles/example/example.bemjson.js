6886fac1e65c3487e553886ab0112206d5b2be99
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-03-09 15:43:18 +0300

    Improve bundle bemjson

--- bundles/example/example.bemjson.js
+++ bundles/example/example.bemjson.js
@@ -1,15 +1,4 @@
 module.exports = {
-    block: 'b-page',
-    head: [
-        {
-            tag: 'meta',
-            attrs: { 'http-equiv': 'content-type', content: 'text/html; charset=utf-8' }
-        },
-        { elem: 'css', url: 'example.css'}
-    ],
-    content: [
-        { block: 'example' },
-        {block: 'i-jquery', mods: {version: 'default'}},
-        { elem: 'js', url: 'example.js'}
-    ]
+    block: 'i-jquery',
+    mods: {version: 'default'}
 };
